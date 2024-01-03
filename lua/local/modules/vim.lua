94cf244fd9f6844f8216714a93d2ce6ca8bed21e
Anatolii Sakhnik <sakhnik@gmail.com>
2024-01-03 16:19:16 +0200

    Remove prices from ledger invocation

--- lua/local/modules/vim.lua
+++ lua/local/modules/vim.lua
@@ -35,7 +35,7 @@ return {
 
     vim.g.ledger_bin = 'ledger'
     vim.g.ledger_date_format = '%Y-%m-%d'
-    vim.g.ledger_extra_options = '--pedantic --explicit --price-db prices.db --date-format ' .. vim.g.ledger_date_format
+    vim.g.ledger_extra_options = '--pedantic --explicit --date-format ' .. vim.g.ledger_date_format
     vim.g.ledger_align_at = 45
     vim.g.ledger_default_commodity = '₴'
     vim.g.ledger_commodity_before = 0
