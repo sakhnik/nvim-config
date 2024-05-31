fa10cf2b56d1f37add03242cb75e33a32818eabd
Anatolii Sakhnik <sakhnik@gmail.com>
2024-05-31 15:22:38 +0300

    Map <esc> only in quickfix buffer

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -1,2 +1,2 @@
 
-vim.api.nvim_set_keymap('n', '<esc>', ':cclose<cr>', { noremap = true, desc = 'Close quickfix window'})
+vim.api.nvim_buf_set_keymap(vim.api.nvim_get_current_buf(), 'n', '<esc>', ':cclose<cr>', { noremap = true, desc = 'Close quickfix window'})
