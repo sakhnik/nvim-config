d143fca5c336670339363c7be47b5310f3f4e23a
Anatolii Sakhnik <sakhnik@gmail.com>
2024-06-04 15:42:45 +0300

    Move make-async into own repository

--- lua/local/modules/quickfix.lua
+++ lua/local/modules/quickfix.lua
@@ -1,9 +1,11 @@
 return {
   plugins = {
     'yorickpeterse/nvim-pqf';
+    'sakhnik/make-async.nvim';
   },
 
   setup = function()
     require'pqf'.setup()
+    require'make-async'.setup()
   end
 }
