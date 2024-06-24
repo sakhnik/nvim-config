a63156e2f39e2de37ebc05a3f75b392f48b46f23
Anatolii Sakhnik <sakhnik@gmail.com>
2024-06-24 17:04:31 +0300

    Add better digraphs with <c-k><c-k>

--- lua/local/modules/mytelescope.lua
+++ lua/local/modules/mytelescope.lua
@@ -7,19 +7,21 @@ C.plugins = {
   'nvim-telescope/telescope-project.nvim';
   'nvim-telescope/telescope-live-grep-args.nvim';
   'nvim-telescope/telescope-ui-select.nvim';
+  'protex/better-digraphs.nvim';
 }
 
 function C.setup()
-  local noremap_silent = {noremap = true, silent = true}
   local tsbi = require'telescope.builtin'
-  vim.keymap.set('n', '<leader>f', tsbi.builtin, noremap_silent)
-  vim.keymap.set('n', '<leader>ff', tsbi.find_files, noremap_silent)
-  vim.keymap.set('n', '<leader>fg', tsbi.git_files, noremap_silent)
+  vim.keymap.set('n', '<leader>f', tsbi.builtin, {noremap = true, silent = true, desc = 'Telescope: select a finder'})
+  vim.keymap.set('n', '<leader>ff', tsbi.find_files, {noremap = true, silent = true, desc = 'Telescope: a file'})
+  vim.keymap.set('n', '<leader>fg', tsbi.git_files, {noremap = true, silent = true, desc = 'Telescope: git file'})
   --vim.keymap.set('n', '<leader>gg', tsbi.live_grep, noremap_silent)
-  vim.keymap.set("n", "<leader>gg", require'telescope'.extensions.live_grep_args.live_grep_args, noremap_silent)
-  vim.keymap.set('n', '<leader>fc', tsbi.current_buffer_fuzzy_find, noremap_silent)
-  vim.keymap.set('n', '<leader>fh', tsbi.command_history, noremap_silent)
-  vim.keymap.set('n', '<leader>fp', function() require'telescope'.extensions.project.project{} end, noremap_silent)
+  vim.keymap.set("n", "<leader>gg", require'telescope'.extensions.live_grep_args.live_grep_args,
+    {noremap = true, silent = true, desc = 'Telescope: live grep'})
+  vim.keymap.set('n', '<leader>fc', tsbi.current_buffer_fuzzy_find, {noremap = true, silent = true, desc = 'Telescope: current buffer'})
+  vim.keymap.set('n', '<leader>fh', tsbi.command_history, {noremap = true, silent = true, desc = 'Telescope: command history'})
+  vim.keymap.set('n', '<leader>fp', function() require'telescope'.extensions.project.project{} end,
+    {noremap = true, silent = true, desc = 'Telescope: project'})
 
   local lga_actions = require"telescope-live-grep-args.actions"
 
@@ -51,7 +53,15 @@ function C.setup()
   }
 
   require'telescope'.load_extension("live_grep_args")
-  require("telescope").load_extension("ui-select")
+  require'telescope'.load_extension("ui-select")
+
+  require'better-digraphs'
+  vim.keymap.set('i', '<C-k><C-k>', function() require'better-digraphs'.digraphs("insert") end,
+    {noremap = true, silent = true, desc = 'Insert a digraph'})
+  vim.keymap.set('n', 'r<C-k><C-k>', function() require'better-digraphs'.digraphs("normal") end,
+    {noremap = true, silent = true, desc = 'Replace with a digraph'})
+  vim.keymap.set('v', 'r<C-k><C-k>', function() require'better-digraphs'.digraphs("visual") end,
+    {noremap = true, silent = true, desc = 'Replace with a digraph'})
 end
 
 return C
