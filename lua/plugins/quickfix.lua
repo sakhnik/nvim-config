b59c238b049f46bda850cea38980c2908dd32eaf
Anatolii Sakhnik <sakhnik@gmail.com>
2024-12-14 20:23:46 +0200

    Configure lazy load of nvim-gdb and make-async

--- lua/plugins/quickfix.lua
+++ lua/plugins/quickfix.lua
@@ -1,5 +1,20 @@
 return {
   { 'yorickpeterse/nvim-pqf', opts = {} },
-  { 'sakhnik/make-async.nvim', opts = {} },
-  { 'sakhnik/nvim-gdb' },
+
+  {
+    'sakhnik/make-async.nvim',
+    opts = {},
+    cmd = { 'Make', 'X' },
+    keys = { '<leader>mm' },
+  },
+
+  {
+    'sakhnik/nvim-gdb',
+    cmd = {
+      'GdbStart', 'GdbStartLLDB', 'GdbStartPDB', 'GdbStartBashDB', 'GdbStartRR',
+    },
+    keys = {
+      '<leader>dd', '<leader>db', '<leader>dp', '<leader>db', '<leader>dr'
+    }
+  },
 }
