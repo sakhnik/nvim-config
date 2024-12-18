884e90bac5ce36a239c0428ada0830038ffc0981
Anatolii Sakhnik <sakhnik@gmail.com>
2024-12-18 21:58:18 +0200

    Lazy loading of FTerm

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -1,22 +1,23 @@
+local function get_shell()
+  if vim.fn.has('win32') == 1 then
+    return "powershell"
+  end
+  return os.getenv('SHELL')
+end
+
 return {
   {
     'numToStr/FTerm.nvim',
-    config = function()
-      local function get_shell()
-        if vim.fn.has('win32') == 1 then
-          return "powershell"
-        end
-        return os.getenv('SHELL')
-      end
-
-      require'FTerm'.setup({ cmd = get_shell(), })
-
-      -- Example keybindings
-      vim.keymap.set('n', '<A-i>', function() require("FTerm").toggle() end, {})
-      vim.keymap.set('t', '<A-i>', function()
+    opts = {
+      cmd = get_shell(),
+      border = 'rounded',
+    },
+    keys = {
+      { '<A-i>', require"FTerm".toggle },
+      { '<A-i>', function()
         vim.api.nvim_input('<C-\\><C-n>')
         require("FTerm").toggle()
-      end, {})
-    end
+      end, mode = 't'}
+    },
   }
 }
