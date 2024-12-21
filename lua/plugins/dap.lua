763fc2afafbfa7c1a48aa2e472481625233c0c35
Anatolii Sakhnik <sakhnik@gmail.com>
2024-12-21 20:33:18 +0200

    Configure DAP keymaps like in nvim-gdb

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,17 +1,71 @@
 
---function C.setup()
---
---  local widgets = function() return require'dap.ui.widgets' end
---
---  -- TODO: activate mapping together with DAP UI
---  vim.keymap.set({'n', 'v'}, '<Leader>dh', function() widgets().hover() end, {noremap = true, desc = 'DAP hover'})
---  vim.keymap.set({'n', 'v'}, '<Leader>dp', function() widgets().preview() end, {noremap = true, desc = 'DAP preview'})
---  vim.keymap.set('n', '<Leader>df', function() widgets().centered_float(widgets().frames) end, {noremap = true, desc = 'DAP frames'})
---  vim.keymap.set('n', '<Leader>ds', function() widgets().centered_float(widgets().scopes) end, {noremap = true, desc = 'DAP scopes'})
---
---end
---
---return C
+local function create_dap_hover()
+  local view = require("dap.ui.widgets").hover()
+
+  -- Close the hover when the buffer is left
+  local api = vim.api
+  api.nvim_create_autocmd("BufLeave", {
+    callback = function()
+      if api.nvim_win_is_valid(view.win) then
+        api.nvim_win_close(view.win, true)
+      end
+    end,
+    buffer = view.buf,
+    once = true
+  })
+end
+
+-- Left click to print the symbol (hover)
+local function on_left_click()
+  local mpos = vim.fn.getmousepos()
+  if vim.api.nvim_win_is_valid(mpos.winid) then
+    vim.api.nvim_set_current_win(mpos.winid)
+    vim.api.nvim_win_set_cursor(mpos.winid, {mpos.line, mpos.column - 1})
+  end
+  create_dap_hover()
+end
+
+local function configure_dap()
+  local dap = require('dap')
+  local timer_id = -1
+
+  local function restore_keymaps()
+    vim.keymap.del('n', '<LeftMouse>')
+    vim.keymap.del('n', '<f4>')
+    vim.keymap.del('n', '<f5>')
+    vim.keymap.del('n', '<f8>')
+    vim.keymap.del('n', '<f9>')
+    vim.keymap.del('n', '<f10>')
+    vim.keymap.del('n', '<f11>')
+    vim.keymap.del('n', '<f12>')
+  end
+
+  local function check_restore_keymaps()
+    if not next(dap.sessions()) then
+      restore_keymaps()
+      vim.fn.timer_stop(timer_id)
+      timer_id = -1
+    end
+  end
+
+  dap.listeners.after['event_initialized']['me'] = function()
+    if timer_id == -1 then
+      -- Set keymaps like in nvim-gdb
+      vim.keymap.set('n', '<f4>', require'dap'.run_to_cursor, { noremap = true, silent = true })
+      vim.keymap.set('n', '<f5>', require'dap'.continue, { noremap = true, silent = true })
+      vim.keymap.set('n', '<f8>', require'dap'.set_breakpoint, { noremap = true, silent = true })
+      vim.keymap.set('n', '<f9>', create_dap_hover, { noremap = true, silent = true })
+      vim.keymap.set('n', '<f10>', require'dap'.step_over, { noremap = true, silent = true })
+      vim.keymap.set('n', '<f11>', require'dap'.step_into, { noremap = true, silent = true })
+      vim.keymap.set('n', '<f12>', require'dap'.step_out, { noremap = true, silent = true })
+      vim.keymap.set('n', '<LeftMouse>', on_left_click, { noremap = true, silent = true })
+
+      timer_id = vim.fn.timer_start(1000, check_restore_keymaps)
+    end
+  end
+
+  dap.listeners.after['event_terminated']['me'] = check_restore_keymaps
+end
 
 return {
   {
@@ -36,6 +90,8 @@ return {
     },
 
     config = function()
+      configure_dap()
+
       if vim.fn.executable('gdb') == 1 then
         require('dapcfg.cpp')
       end
