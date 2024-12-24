121ab53d5b6274598efa68f95d7f3147e7faf3b6
Anatolii Sakhnik <sakhnik@gmail.com>
2024-12-24 20:48:33 +0200

    Specify leetcode dependency on nvim-treesitter

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,11 +1,12 @@
 return {
   "kawre/leetcode.nvim",
-  build = ":TSUpdate html",
+  build = ':TSUpdate html',
   dependencies = {
     "nvim-telescope/telescope.nvim",
     -- "ibhagwan/fzf-lua",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
+    'nvim-treesitter/nvim-treesitter'
   },
   cmd = 'Leet',
   opts = {
