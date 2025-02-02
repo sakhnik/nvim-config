d3248b0d58e3d211dd3e52e1c2809a710253f50c
Anatolii Sakhnik <sakhnik@gmail.com>
2025-02-02 19:58:00 +0200

    Configure nvim-dap-python

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -116,6 +116,9 @@ return {
       {
         'mfussenegger/nvim-dap-python',
         ft = 'python',
+        config = function()
+          require'dap-python'.setup('python3')
+        end,
       },
     },
 
