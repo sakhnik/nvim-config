6c55c9bad0d0ddf7374960dc8e5d84277b5dd3f6
Anatolii Sakhnik <sakhnik@gmail.com>
2025-04-03 12:36:08 +0300

    Turn off blink.cmp in cmdline

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -41,6 +41,8 @@ return {
 
       signature = { enabled = true },
 
+      cmdline = { enabled = false },
+
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
