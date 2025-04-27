e6494c65b8192b207574d4acaf9fa66f47384aea
Anatolii Sakhnik <sakhnik@gmail.com>
2025-04-27 15:43:45 +0300

    Fix telekasten keymap clash

--- lua/plugins/wiki.lua
+++ lua/plugins/wiki.lua
@@ -15,7 +15,7 @@ return {
       { "<leader>zf", "<cmd>Telekasten find_notes<CR>", desc = 'Telekasten find notes' },
       { "<leader>zg", "<cmd>Telekasten search_notes<CR>", desc = 'Telekasten search notes' },
       { "<leader>zd", "<cmd>Telekasten goto_today<CR>", desc = 'Telekasten goto today' },
-      { "<leader>zz", "<cmd>Telekasten follow_link<CR>", desc = 'Telekasten follow link' },
+      --{ "<leader>zz", "<cmd>Telekasten follow_link<CR>", desc = 'Telekasten follow link' },
       { "<leader>zn", "<cmd>Telekasten new_note<CR>", desc = 'Telekasten new note' },
       { "<leader>zc", "<cmd>Telekasten show_calendar<CR>", desc = 'Telekasten show calendar' },
       { "<leader>zb", "<cmd>Telekasten show_backlinks<CR>", desc = 'Telekasten show backlinks' },
