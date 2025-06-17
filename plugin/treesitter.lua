31fe1db3788b76c7b387df0d265e2dd0c5f3d97f
Anatolii Sakhnik <sakhnik@gmail.com>
2025-06-17 13:25:57 +0300

    Install treesitter parsers automatically, and use for highlights

---
+++ plugin/treesitter.lua
@@ -0,0 +1,24 @@
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '*' },
+  callback = function(ev)
+    local nts = require'nvim-treesitter'
+
+    local ft = ev.match
+    local lang = vim.treesitter.language.get_lang(ft)
+
+    if lang and vim.treesitter.language.add(lang) then
+      local installed = nts.get_installed('parsers')
+      for _, installed_lang in ipairs(installed) do
+        if installed_lang == lang then
+          if vim.treesitter.query.get(lang, 'highlights') then
+            pcall(vim.treesitter.start)
+            vim.bo[ev.buf].indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
+          end
+          break
+        end
+      end
+
+      pcall(nts.install, lang);
+    end
+  end,
+})
