511c11c6f6ac4af7f6df2124742a00c8dfc32efb
Anatolii Sakhnik <sakhnik@gmail.com>
2025-12-11 12:28:49 +0200

    Install any rusticean version

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,3 +1,3 @@
 vim.pack.add {
-  { src = 'https://github.com/mrcjkb/rustaceanvim', version = vim.version.range('^5'), },
+  { src = 'https://github.com/mrcjkb/rustaceanvim', version = vim.version.range('*'), },
 }
