08a71aba16ca877de606b7f8d3160e3238433ca0
Anatolii Sakhnik <sakhnik@gmail.com>
2025-12-25 13:45:42 +0200

    Complete with <tab> in all files

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -5,3 +5,22 @@ vim.o.complete = "o,.,w,b,u"
 vim.o.completeopt = "fuzzy,menuone,noselect,popup"
 vim.o.pumheight = 7
 vim.o.pummaxwidth = 80
+
+
+-- Insert-mode <Tab>: completion next
+vim.keymap.set("i", "<Tab>", function()
+  if vim.fn.pumvisible() == 1 then
+    return '<c-n>'
+  else
+    return '<tab>'
+  end
+end, { expr = true })
+
+-- Insert-mode <Tab>: completion prev
+vim.keymap.set("i", "<s-Tab>", function()
+  if vim.fn.pumvisible() == 1 then
+    return '<c-p>'
+  else
+    return '<s-tab>'
+  end
+end, { expr = true })
