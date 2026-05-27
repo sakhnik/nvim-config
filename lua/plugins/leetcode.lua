27dd2f1404414846fe330de3baf4441412cdd078
Anatolii Sakhnik <sakhnik@gmail.com>
2026-05-27 11:46:43 +0300

    Enable leetcode

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,16 +1,8 @@
-return {
-  "kawre/leetcode.nvim",
-  build = ':TSUpdate html',
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-    -- "ibhagwan/fzf-lua",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    'nvim-treesitter/nvim-treesitter'
-  },
-  cmd = 'Leet',
-  opts = {
-    -- configuration goes here
-    lang = vim.env.LEET_LANG and vim.env.LEET_LANG or 'cpp'
-  },
+vim.pack.add {
+  { src = 'https://github.com/kawre/leetcode.nvim', },
+  { src = 'https://github.com/MunifTanjim/nui.nvim', },
+}
+
+require'leetcode'.setup {
+  lang = vim.env.LEET_LANG and vim.env.LEET_LANG or 'cpp'
 }
