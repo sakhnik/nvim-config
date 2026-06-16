7d00406a8598e34cb4c0d1ef49f00b46344de4b5
Anatolii Sakhnik <sakhnik@gmail.com>
2026-06-16 21:20:51 +0300

    Try quicker.nvim

--- lua/plugins/quickfix.lua
+++ lua/plugins/quickfix.lua
@@ -1,8 +1,8 @@
 vim.pack.add {
-  { src = 'https://github.com/yorickpeterse/nvim-pqf', },
+  { src = 'https://github.com/stevearc/quicker.nvim', },
   { src = 'https://github.com/sakhnik/make-async.nvim', },
   { src = 'https://github.com/sakhnik/nvim-gdb', },
 }
 
-require'pqf'.setup {}
+require'quicker'.setup {}
 require'make-async'.setup {}
